a91b3783fe66657a99bfa0eac1b65de89914c373
Jason Hanna <jason.hanna01@gmail.com>
2016-12-15 16:18:17 -0700

    add mirror_row function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,13 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
   end
 
   def hash_input(input) do
@@ -15,6 +22,13 @@ defmodule Identicon do
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    #|> mirror_row
+
   end
 
 end
